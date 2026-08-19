bd128d2c705c7121601e128488355fb2443cf85c
Abinaya120706 <abinaya.s1276@gmail.com>
2026-08-19 15:35:22 +0530

    Add variables and print statements in variables.py
    Add Python variables practice

---
+++ Module-1-AI-Python/variables.py
@@ -0,0 +1,13 @@
+# Codomax AI & ML Internship
+# Module 1 - Introduction to AI & Python
+# Python Variables
+
+name = "Abinaya"
+age = 20
+department = "Artificial Intelligence and Data Science"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Department:", department)
+print("Student:", is_student)
